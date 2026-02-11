63326b3309e7edcbbac7cd8186ca4e677324222b
Ava Clifton <ava.or.c@gmail.com>
2026-02-11 15:04:37 +1100

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,13 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     bind \t complete
+    bind \cl accept-autosuggestion
+    bind \cu 'echo ""; cd ..; commandline -f repaint'
+    alias forall='xargs -L1 -d "\n"'
 
     alias nvim=~/nvim.appimage
     alias lua-language-server=~/ava/lua_language_server/lua-language-server-3.15.0-linux-x64/bin/lua-language-server
     alias nvimnote='nvim ~/data/notes/note__$(date +%Y_%m_%d__%H_%M_%S).md'
     source ~/.venv/bin/activate.fish
     mount /mnt/orgmode/ 2&> /dev/null
-    alias org='cd /mnt/orgmode/orgfiles && ls'
+    alias org='cd /mnt/orgmode/orgfiles'
     alias s='git status'
     alias d='git diff'
 end
